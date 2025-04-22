95f6f8c1039079ad5cbc687910e24cc6c11b3164
AgostoSound <agostosound@gmail.com>
2025-04-22 23:36:02 +0200

    Clean data, all columns are bools or standarized floats.

--- notebooks/02_cleaning.py
+++ notebooks/02_cleaning.py
@@ -4,8 +4,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 
 
-def clean_data(df: pd.DataFrame, categorical_cols: list, binary_mappings: dict) -> pd.DataFrame:
-    df.fillna(df.mean(numeric_only=True), inplace=True)  # Replace null values with the mean of the column.
+def clean_data(df: pd.DataFrame, binary_mappings: dict, categorical_cols: list) -> pd.DataFrame:
     df.drop_duplicates(inplace=True)  # Drop duplicates.
 
     # Replace binary columns to boolean columns.
@@ -13,24 +12,36 @@ def clean_data(df: pd.DataFrame, categorical_cols: list, binary_mappings: dict) 
         if col in df.columns:
             df[col] = df[col].map(mapping)
 
-    # Convert categorical variables to binary variables.
+    # Normalize categorical_cols.
     df = pd.get_dummies(df, columns=categorical_cols, drop_first=True, dtype=bool)
 
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df['TotalCharges'] = df['TotalCharges'].astype(float)
+    df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].mean())
     scaler = StandardScaler()
-    numerical_columns = df.select_dtypes(include=[np.number]).columns  # Get numerical cols.
-    df[numerical_columns] = scaler.fit_transform(df[numerical_columns])  # Automatically scales.
+    df[['MonthlyCharges', 'TotalCharges']] = scaler.fit_transform(df[['MonthlyCharges', 'TotalCharges']])
+
+    df['tenure_group'] = pd.cut(df['tenure'], bins=[0, 12, 24, 48, 60, np.inf],
+                                labels=['0-12', '12-24', '24-48', '48-60', '60+'])
+    df = pd.get_dummies(df, columns=['tenure_group'], drop_first=True, dtype=bool)
+
+    # Drop unimportant columns.
+    unimportant_cols = ['customerID', 'tenure']
+    df.drop(columns=unimportant_cols, inplace=True)
 
     print(' ')
-    print('DATAFRAME')
-    print(df)
+    print('df')
+    print(df[['MonthlyCharges', 'TotalCharges']])
+    print(' ')
+    print(df.dtypes)
     print(' ')
 
     return df
 
 
-def prepare_data(file_path: str, categorical_cols: list, binary_mappings: dict) -> pd.DataFrame:
+def prepare_data(file_path: str, binary_mappings: dict, categorical_cols: list) -> pd.DataFrame:
     df = pd.read_csv(file_path)  # Upload dataset.
-    df = clean_data(df, categorical_cols, binary_mappings)  # Clean and preformat.
+    df = clean_data(df, binary_mappings, categorical_cols)  # Clean and preformat.
 
     # Split the data into training and testing sets.
     x = df.drop('Churn', axis=1)  # Features without the target variable.
@@ -39,24 +50,16 @@ def prepare_data(file_path: str, categorical_cols: list, binary_mappings: dict) 
     return train_test_split(x, y, test_size=0.2, random_state=42)
 
 
-
-categorical_cols = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling']
 binary_mappings = {
     'gender': {'Female': False, 'Male': True},
-    'owns_house': {'No': False, 'Yes': True}
+    'SeniorCitizen': {0: False, 1: True},
+    'Partner': {'No': False, 'Yes': True},
+    'Dependents': {'No': False, 'Yes': True},
+    'PhoneService': {'No': False, 'Yes': True},
+    'PaperlessBilling': {'No': False, 'Yes': True},
+    'Churn': {'No': False, 'Yes': True},
 }
-x_train, x_test, y_train, y_test = prepare_data("data/telco_churn.csv", categorical_cols, binary_mappings)
+categorical_cols = ['MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+                    'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaymentMethod']
 
-
-
-
-
-
-print(' ')
-print('First 5 rows of the training set.')
-print(x_train.head())  # First 5 rows of the training set.
-print(' ')
-print(' ')
-print('First 5 rows of the target variable.')
-print(y_train.head())  # First 5 rows of the target variable.
-print(' ')
+x_train, x_test, y_train, y_test = prepare_data("data/telco_churn.csv", binary_mappings, categorical_cols)
